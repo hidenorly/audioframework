099f6a21e678a0e54ce854dcf2416d1587f78043
hidenorly <twitte.harold@gmail.com>
2021-04-16 00:17:20 +0900

    Add Buffer, Stream

---
+++ Stream.hpp
@@ -0,0 +1,30 @@
+/* 
+  Copyright (C) 2021 hidenorly
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef __STREAM_H__
+#define __STREAM_H__
+
+#include "Buffer.hpp"
+
+class Stream
+{
+public:
+  bool isEof(void);
+  int read(ByteBuffer& buf);
+  ByteBuffer* read(void);
+};
+
+#endif /* __STREAM_H__ */
